7060d34a6adaf3c53429f2db7e0f56e6ee42d4e4
Mingyang Li <tslmy@outlook.com>
2024-02-20 00:37:33 -0800

    remove callback

--- demo_for_issue.py
+++ demo_for_issue.py
@@ -1,19 +1,14 @@
+from llama_index.core import Settings
 from llama_index.core.agent import ReActAgent
 from llama_index.core.callbacks import CallbackManager, LlamaDebugHandler
-
-callback_handlers = [LlamaDebugHandler()]
-callback_manager = CallbackManager(callback_handlers)
-from llama_index.core import Settings
 from llama_index.llms.ollama import Ollama
 
 Settings.llm = Ollama(
     model="zephyr:7b-beta",
     timeout=600,  # secs
     streaming=True,
-    callback_manager=callback_manager,
-    additional_kwargs={"stop": ["Observation:"], "seed": 42},
+    additional_kwargs={"stop": ["Observation:"], "seed": 42, "temperature": 0.01},
 )
-Settings.callback_manager = callback_manager
 Settings.embed_model = "local"
 
 from tool_for_my_notes import make_tool as make_tool_for_my_notes
@@ -30,7 +25,6 @@ agent = ReActAgent.from_tools(
     tools=all_tools,
     verbose=True,
     react_chat_formatter=chat_formatter,
-    callback_manager=callback_manager,
 )
 
 QUERY = "Name a type of drink that I enjoy, and then look up its country of origin. Be concise."
